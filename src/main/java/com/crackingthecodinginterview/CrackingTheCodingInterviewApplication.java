4a0aafd542641121340b549541d55189a99cc097
pankajpkb <1pankaj4@gmail.com>
2019-10-12 14:32:59 +0530

    Ádded Çhapter 1 solutions

---
+++ src/main/java/com/crackingthecodinginterview/CrackingTheCodingInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.crackingthecodinginterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrackingTheCodingInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrackingTheCodingInterviewApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/crackingthecodinginterview/chapter1/Question1.java
@@ -0,0 +1,37 @@
+package com.crackingthecodinginterview.chapter1;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * IsUnique: Implement an algorithm to determine if a string has all unique characters.
+ * What if you cannot use additional data structures.
+ * 
+ * */
+public class Question1 {
+
+	private static Scanner in = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		System.out.println("Enter a word to check if it has unique characters:");
+		String word = in.next();
+		System.out.println(isUnique(word));
+	}
+
+	private static boolean isUnique(String word) {
+		int[] chars = new int[128];
+		for(int i=0; i< word.length();i++) {
+			int ch = word.charAt(i);
+			if(chars[ch] != 0)
+				return false;
+			chars[ch] = 1;
+		}
+		return true;
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		in.close();
+	}
+
+}
